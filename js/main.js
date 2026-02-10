cf5525d987611d76c79075f62c88c302ec04126f
estherluizacouto-glitch <estherluizacouto@gmail.com>
2026-02-10 04:20:53 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,10 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
   async function loginWithGoogle() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
-      options: {
-        redirectTo: window.location.origin + '/KiwiBeats/'
-      }
-    })
+      redirectTo: window.location.origin + '/KiwiBeats/'
+    });
   
     if (error) {
       console.error('Erro no login:', error.message)
