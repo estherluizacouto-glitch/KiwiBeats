75777477d025b416c26b8e6d28015d984ebab22d
estherluizacouto-glitch <estherluizacouto@gmail.com>
2026-02-10 02:32:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+  'https://gevwfciirevgbrzklrpp.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdldndmY2lpcmV2Z2JyemtscnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTg0NTgsImV4cCI6MjA4NjIzNDQ1OH0.auWkhHSUy9mzk-2U0AYExgzf90MQbZ6PPd98VhJMN4w'
+)
+
 document.addEventListener('DOMContentLoaded', () => {
 
   const textarea = document.querySelector('.cta-box textarea');
