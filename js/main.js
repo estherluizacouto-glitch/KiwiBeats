d9299784449abc9c0b51c5bc5b2b074a9b6f5f6f
estherluizacouto-glitch <estherluizacouto@gmail.com>
2026-02-12 12:10:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -193,4 +193,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  const sidebarToggle = document.getElementById("sidebarToggle");
+  const sidebar = document.getElementById("sidebar");
+  
+  sidebarToggle.addEventListener("click", () => {
+      sidebar.classList.toggle("open");
+  });
+
+
 });
