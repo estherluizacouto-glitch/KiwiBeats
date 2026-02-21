274e84c87511ae2cf17631784b910b6ff4ceb765
estherluizacouto-glitch <estherluizacouto@gmail.com>
2026-02-21 02:23:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdldndmY2lpcmV2Z2JyemtscnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTg0NTgsImV4cCI6MjA4NjIzNDQ1OH0.auWkhHSUy9mzk-2U0AYExgzf90MQbZ6PPd98VhJMN4w'
 )
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+
+  
+const { data: { session } } = await supabase.auth.getSession();
+console.log("SESSION AT LOAD:", session);
+
 
 lucide.createIcons();
   
