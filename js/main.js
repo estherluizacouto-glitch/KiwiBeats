ad2194e90b304115ad987bfe2a317f06bd4b0c0d
estherluizacouto-glitch <estherluizacouto@gmail.com>
2026-02-27 02:09:57 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,6 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdldndmY2lpcmV2Z2JyemtscnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTg0NTgsImV4cCI6MjA4NjIzNDQ1OH0.auWkhHSUy9mzk-2U0AYExgzf90MQbZ6PPd98VhJMN4w'
 )
 
-
-const supabase = createClient(...);
-
 window.supabaseClient = supabase;
 
 
