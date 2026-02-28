9631c1c3d60ea7a78fff065e992dae3b2f343bd0
estherluizacouto-glitch <estherluizacouto@gmail.com>
2026-02-28 04:32:04 -0300

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,8 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+  'https://gevwfciirevgbrzklrpp.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdldndmY2lpcmV2Z2JyemtscnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTg0NTgsImV4cCI6MjA4NjIzNDQ1OH0.auWkhHSUy9mzk-2U0AYExgzf90MQbZ6PPd98VhJMN4w'
+);
+
+export default supabase;
