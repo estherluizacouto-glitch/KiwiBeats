cebaff671aa298401e43a34c682a840d1c64ddb1
estherluizacouto-glitch <estherluizacouto@gmail.com>
2026-02-28 04:30:27 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabase = createClient(
-  'https://gevwfciirevgbrzklrpp.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdldndmY2lpcmV2Z2JyemtscnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTg0NTgsImV4cCI6MjA4NjIzNDQ1OH0.auWkhHSUy9mzk-2U0AYExgzf90MQbZ6PPd98VhJMN4w'
-)
-
-window.supabaseClient = supabase;
+import supabase from './supabaseClient.js'
 
 
 document.addEventListener('DOMContentLoaded', async () => {
